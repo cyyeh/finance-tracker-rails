d6d2281ee00551d8cfc555df97958452fb5b8a77
cyyeh <chihyu.jimmy.yeh@gmail.com>
2018-04-15 00:34:59 +0800

    add search friend form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
   resources :users, only: [:show]
   resources :friendships
 end
