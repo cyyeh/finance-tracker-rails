2b7ba304d4a1946858dad0f275b774ddceffe0f1
cyyeh <chihyu.jimmy.yeh@gmail.com>
2018-04-14 21:36:33 +0800

    add model userstock and implement adding stock to user

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  resources :user_stocks, only: [:create]
 end
